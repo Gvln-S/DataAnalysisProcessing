ad844840c15108736625b8eea571068b3283a12d
Gvln-S <santis.paez2414@outlook.com>
2025-05-19 19:52:46 -0500

    feat: basic web app with python

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import numpy as np
 
 app = Flask(__name__)
 
+# Cargar el modelo
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+        age = int(request.form['age'])
+        sex = int(request.form['sex'])  # 0: mujer, 1: hombre
+        education_level = int(request.form['education_level'])  # codificado
+        occupation = int(request.form['occupation'])  # codificado
+        hours_worked = float(request.form['hours_worked'])
+
+        features = np.array([[age, sex, education_level, occupation, hours_worked]])
+
+        return f"Error en la predicción"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
